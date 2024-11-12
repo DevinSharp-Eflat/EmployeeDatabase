48d79cbc4f56b80699ebe2b49712e3e94edf462d
Devin Sharp <sharpdevin467@gmail.com>
2024-11-11 17:13:14 -0800

    add nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,20 @@
+import * as React from 'react';
+import { Button, Divider, Stack } from '@mui/material';
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <>
+    <div>Nav</div><Stack direction={"row"} spacing={20} sx={{ alignContent: 'center' }}>
+        <Button variant='outlined'
+        onClick={() => { window.location.href = "./";
+        }}>Candidate Search</Button>
+        <Divider orientation="vertical" variant="middle" />
+        <Button variant='outlined'
+        onClick={() => { window.location.href = "./SavedCandidates";
+        }}>Saved Candidates</Button>
+    </Stack>
+    </>
   )
 };
 
